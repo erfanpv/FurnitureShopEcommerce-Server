4ac1a957cb0822fcb83ba06761e7404eb39e87f2
erfanpv <erfanpv786@gmail.com>
2024-08-26 14:30:13 +0530

    Handle All Products

---
+++ src/controllers/productController.js
@@ -0,0 +1,14 @@
+import productDb from "../models/productSchem";
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const productsList = await productDb.find();
+    if (!productsList) {
+      return res.status(404).json({ message: "Products Not found" });
+    }
+    res.status(200).json(productsList);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error fetching Products" });
+  }
+};
