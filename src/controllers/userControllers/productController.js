5bac8d005de778a804b1e36dcca7996a2f77bc7c
erfanpv <erfanpv786@gmail.com>
2024-08-26 23:24:45 +0530

    Handle Products With id and Category

--- src/controllers/userControllers/productController.js
+++ src/controllers/userControllers/productController.js
@@ -1,4 +1,4 @@
-import productDb from "../../models/schemas/productSchem";
+import productDb from "../../models/schemas/productSchema.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -8,7 +8,34 @@ export const getAllProducts = async (req, res) => {
     }
     res.status(200).json(productsList);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error fetching Products" });
+    res.status(500).json({ message: `Error fetching Products - ${error.message}`});
   }
 };
+
+export const getProductWithId = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const productById = await productDb.findById(productId);
+
+    if (!productById) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(productById);
+  } catch (error) {
+    res.status(500).json({ message: `Error fetching product - ${error.message}` });
+  }
+};
+
+export const getproductWithCategory = async (req,res) => {
+  try {
+    const categoryId = req.params.categoryname;
+    const productsWithCategory = await productDb.find({ category:categoryId});
+
+    if (productsWithCategory.length === 0) {
+      return res.status(404).json({ message: "Categories not found" });
+    }
+    return res.status(200).json(productsWithCategory)
+  }catch (error) {
+    return res.status(500).json({message:`Error fetching Categories - ${error.message}`})
+  }
+}
