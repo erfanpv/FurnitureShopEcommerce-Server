47e76d111a541ac79bd4ad11d8f37838f00161c8
erfanpv <erfanpv786@gmail.com>
2024-08-27 10:40:31 +0530

    setup route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {signUp,login,} from "../controllers/userControllers/userController.js";
 import { getAllProducts, getproductWithCategory, getProductWithId,} from "../controllers/userControllers/productController.js";
+import { addToWishList } from "../controllers/userControllers/wishListController.js";
 
 const userRouter = express.Router();
 
@@ -10,5 +11,7 @@ userRouter.post("/login", login);
 userRouter.get("/products", getAllProducts);
 userRouter.get("/products/:id", getProductWithId);
 userRouter.get("/products/category/:categoryname", getproductWithCategory);
+
+userRouter.post("/:id/wishlist",addToWishList)
 
 export { userRouter };
