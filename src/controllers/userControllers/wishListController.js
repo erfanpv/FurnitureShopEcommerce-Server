9f38aacb585d3c27327c09a65ffd21df4f6f57ea
erfanpv <erfanpv786@gmail.com>
2024-08-31 21:33:16 +0530

    Toggle WishList

--- src/controllers/userControllers/wishListController.js
+++ src/controllers/userControllers/wishListController.js
@@ -1,38 +1,53 @@
 import wishListDb from "../../models/schemas/wishSchema.js";
 
-export const addToWishList = async (req, res) => {
+export const toggleWishListItem = async (req, res) => {
   try {
     const userId = req.params.id;
     const { productId } = req.body;
 
     let wishList = await wishListDb.findOne({ userId });
-    
+
     if (!wishList) {
       wishList = new wishListDb({
         userId,
-        products: [{ productId }], 
+        products: [{ productId }],
       });
 
       await wishList.save();
-      return res.status(200).json({ message: "Wishlist created and item added successfully", wishlistCount: wishList.products.length });
+      return res.status(200).json({
+        success:true,
+        message: "Wishlist created and item added successfully",
+        wishlistCount: wishList.products.length,
+      });
     } else {
-      const productExists = wishList.products.some(
+      const productIndex = wishList.products.findIndex(
         (product) => product.productId.toString() === productId
       );
 
-      if (!productExists) {
+      if (productIndex === -1) {
         wishList.products.push({ productId });
         await wishList.save();
-
-        return res.status(200).json({ message: "Item added to wishlist successfully", wishlistCount: wishList.products.length });
+        return res.status(200).json({
+          success:true,
+          message: "Item added to wishlist successfully",
+          wishlistCount: wishList.products.length,
+        });
       } else {
-        return res.status(200).json({ message: "Product already in wishlist", wishlistCount: wishList.products.length });
+        wishList.products.splice(productIndex, 1);
+        await wishList.save();
+        return res.status(200).json({
+          success:true,
+          message: "Item removed from wishlist successfully",
+          wishlistCount: wishList.products.length,
+        });
       }
     }
   } catch (error) {
-    res.status(500).json({ message: `Error adding item to wishlist - ${error.message}` });
+    res.status(500).json({success:false,message: `Error toggling item in wishlist - ${error.message}`,
+    });
   }
 };
+
 
 export const loadWishListPage = async (req, res) => {
   try {
