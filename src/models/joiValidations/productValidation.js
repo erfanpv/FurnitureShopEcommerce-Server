255959124c0371ff964065d6fb50782565122456
erfanpv <erfanpv786@gmail.com>
2024-08-31 21:34:16 +0530

    Implement Joi Validation

--- src/models/joiValidations/productValidation.js
+++ src/models/joiValidations/productValidation.js
@@ -2,11 +2,13 @@ import Joi from "joi";
 
 const productValidation = Joi.object({
   productName: Joi.string().required(),
-  description: Joi.string().optional().required(),
+  description: Joi.string().required(),
   price: Joi.number().required(),
-  image: Joi.string().optional().uri().required(),
+  image: Joi.string().uri().required(),
   category: Joi.string().required(),
-  stockQuantity: Joi.number().optional().min(0).required(),
+  stockQuantity: Joi.number().required().min(1).required(),
+  is_Listed: Joi.optional(),
+  is_deleted: Joi.optional(),
 });
 
 export default productValidation;
