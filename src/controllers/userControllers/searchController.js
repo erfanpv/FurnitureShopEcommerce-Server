200af99942a9fb4d9debeff69087db481ec47b1a
erfanpv <erfanpv786@gmail.com>
2024-09-04 14:04:45 +0530

    Implement Search Query

---
+++ src/controllers/userControllers/searchController.js
@@ -0,0 +1,24 @@
+import productDb from "../../models/schemas/productSchema.js";
+
+export const searchProducts = async (req, res) => {
+  try {
+    const { searchQuery } = req.body; 
+
+    const results = await productDb.find({
+      $and: [
+        { is_Listed: true }, 
+        {
+          $or: [
+            { productName: { $regex: searchQuery, $options: "i" } },
+            { description: { $regex: searchQuery, $options: "i" } },
+            { category: { $regex: searchQuery, $options: "i" } },
+          ],
+        },
+      ],
+    });
+
+    res.status(200).json({ success: true, message: "Search Success", data: results });
+  } catch (error) {
+    res.status(500).json({ success: false, message: `Error searching products - ${error.message}` });
+  }
+};
