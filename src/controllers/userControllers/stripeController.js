19fd0e3250e213871234a7b4de9bf5e3ecf2b9e8
erfanpv <erfanpv786@gmail.com>
2024-09-30 23:02:29 +0530

    Add Stripe Integration

--- src/controllers/userControllers/stripeController.js
+++ src/controllers/userControllers/stripeController.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import cartDb from "../../models/schemas/cartSchema.js";
 import productDb from "../../models/schemas/productSchema.js";
 import orderDb from "../../models/schemas/orderSchema.js";
-import paymentSession from "../../models/schemas/paymentSession.js";
 
 dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
@@ -15,15 +14,7 @@ export const stripeIntent = async (req, res) => {
   try {
     const userId = req.params.id;
     const { cartId } = req.body;
-    console.log(cartId);
     
-    // let paymentProductData = new Map();
-
-    // if (cartId) {
-    //   paymentProductData.set("cartId", cartId);
-    // } else if (products) {
-    //   paymentProductData.set("products", products);
-    // }
 
     let productDetails;
     let totalAmount = 0;
@@ -69,32 +60,7 @@ export const stripeIntent = async (req, res) => {
         )
       ).filter((item) => item !== null);
     } 
-    // else if (products) {
-    //   const { productId, quantity } = products;
-
-    //   const productDetails = await productDb.findById(productId);
-
-    //   if (productDetails) {
-    //     totalAmount += productDetails.price * quantity;
-
-    //     lineItems.push({
-    //       price_data: {
-    //         currency: "inr",
-    //         product_data: {
-    //           name: productDetails.productName,
-    //           description: productDetails.description,
-    //           images: [productDetails.image],
-    //         },
-    //         unit_amount: Math.round(productDetails.price * 100),
-    //       },
-    //       quantity: quantity,
-    //     });
-    //   } else {
-    //     return res
-    //       .status(400)
-    //       .json({ success: false, error: "Product not found" });
-    //   }
-    // }
+  
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -107,34 +73,7 @@ export const stripeIntent = async (req, res) => {
       cancel_url: "http://localhost:5173/products/cart/mycart",
     });
 
-    // const existingSession = await paymentSession.findOne({ userId });
-
-    // if (existingSession) {
-
-    //   existingSession.paymentData.push({
-    //     sessionId: session.id,
-    //     pa,
-    //     totalAmount,
-    //   });
-    //   await existingSession.save();
-    // } else {
-      
-    //    paymentData = {
-    //     sessionId: session.id,
-    //     cartId: cartId,
-    //     totalAmount,
-    //   }
-      
-
-    //   try {
-    //     await paymentSession.create({
-    //       userId,
-    //       paymentData: [paymentData],
-    //     });
-    //   } catch (error) {
-    //     console.error("Error while creating payment session:", error);
-    //   }
-    // }
+    
     paymentData = {
       sessionId: session.id,
       cartId: cartId,
@@ -158,7 +97,7 @@ export const stripeIntent = async (req, res) => {
 export const successPayment = async (req, res) => {
   try {
     const userId = req.params.id;
-    
+    console.log(userId)
     const [session, lineItems] = await Promise.all([
       stripe.checkout.sessions.retrieve(req.query.session_id, {
         expand: ["payment_intent.payment_method"],
@@ -167,11 +106,7 @@ export const successPayment = async (req, res) => {
     ]);
 
     console.log(session);
-    
-    // const existingSession = await paymentSession.findOne({
-    //   userId,
-    //   "paymentData.sessionId": session.id,
-    // });
+
 
     let cartId;
     let totalAmount = 0;
@@ -179,19 +114,12 @@ export const successPayment = async (req, res) => {
     console.log(paymentData);
 
     if (paymentData) {
-      // const paymentProductData = paymentData?.paymentProductData;
-      // products = paymentProductData.products;
+   
       totalAmount = paymentData?.totalAmount;
       cartId = paymentData?.cartId;
 
     }
-    // else {
-    //   const paymentInfo = existingSession.paymentData.find((data) => data.sessionId === session.id);      
-    //   const paymentProductData = paymentInfo?.paymentProductData;
-    //   totalAmount = paymentInfo?.totalAmount;
-    //   // products = paymentProductData.products;
-    //   cartId = paymentProductData?.cartId;
-    // }
+
 
     if (cartId) {
       
@@ -223,30 +151,11 @@ export const successPayment = async (req, res) => {
 
       await orderData.save();
 
-      //if the order is success the cart is empty
       await cartDb.deleteOne({ _id: new mongoose.Types.ObjectId(cartId) });
     } 
-    // else if (products) {
-    //   let orderData = await orderDb.findOne({ userId });
+    
+    res.redirect("http://localhost:5173/payment/success/payment")
 
-    //   const orderDetails = {
-    //     orderId: session.id,
-    //     products,
-    //     total: totalAmount,
-    //     status: "Placed",
-    //     createdAt: new Date(),
-    //   };
-
-    //   if (!orderData) {
-    //     orderData = new orderDb({ userId, orderDetails: [orderDetails] });
-    //   } else {
-    //     orderData.orderDetails.push(orderDetails);
-    //   }
-
-    //   await orderData.save();
-    // }
-
-    res.status(200).json({success: true,message: "Stripe payment  successfully Completed",});
   } catch (error) {
     res.status(500).json({
       success: false,
